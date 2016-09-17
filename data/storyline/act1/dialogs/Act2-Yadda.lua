9ffd07da651f9e8a748ceb6138f13fca658aec46
Jesusaves <cpntb1@ymail.com>
2016-09-17 17:09:29 -0300

    Continuation of RR South Area works: Rewrite Master Yadda partialy.
    This includes an option to buy Source Books of Light (limited to 10 units).
    It also allows you to craft a Light Saber.
    
    Light Saber was rebalanced to be a suitable quest reward.
    Ammo clip was increased in 50%, it's now a 1 hand weapon, it doesn't drops
    anymore, attack time and reload time were reduced, and damage increased in 5.
    
    Also, master Yadda will tell about the 0xx class, also called “Alpha Class”.
    It's a class of experimental development droids. They're meant to give you a
    challenge while introducing what you can expect on Act 3.
    
    Some texts were rewritten on master Yadda for a better experience.

--- data/storyline/act1/dialogs/Act2-Yadda.lua
+++ data/storyline/act1/dialogs/Act2-Yadda.lua
@@ -18,7 +18,7 @@
 ----------------------------------------------------------------------
 --[[WIKI
 PERSONALITY = { "Arrogant", "Wise" },
-PURPOSE = "$$NAME$$ is a strange monk who follows the path of light. In future may require good Karma to engage conversation with. Gives quest to slay evil viruses. Give Source Book of Light, Light Saber and Light Staff. Sells player Laser Power Pack and small plasma grenades. Is a pacifist with a weak resolve, thus why he sells Tux grenades etc. May summon droids to help Tux in certain quests."
+PURPOSE = "$$NAME$$ is a strange monk who follows the path of light. Options become available as you talk to him. Gives quest to slay evil viruses. Give quests for Source Book of Light, Light Saber and Light Staff. Sells player Laser Power Pack and small plasma grenades. Is a pacifist with a weak resolve, thus why he sells Tux grenades etc. May summon droids to help Tux in certain quests, or to kill tux if he appears to be walking on an evil path, be it because bad karma, or because strange items on his possession."
 WIKI]]--
 
 local Npc = FDrpg.get_npc()
@@ -27,10 +27,15 @@ local Tux = FDrpg.get_tux()
 return {
 	FirstTime = function()
 		Yadda_times=0
+		Yadda_TP_cost=1
+		Yadda_books_bought=0
+		Yadda_vmx_saber=20+(math.random(5,7)*difficulty_level())
+		Npc:says(_"Welcome to my temple of peace. People say I am the wisest man alive on the world, but I think I am the best on the universe!", "NO_WAIT")
+		cli_says(_"And in case you're wondering: ", "NO_WAIT")
 	end,
 
 	EveryTime = function()
-		-- People say I am the wisest man alive on the world, but I think I am the best on the universe!
+		-- 
 		Npc:says(_"Greetings, I'm Master Yadda. I teach the path of light.")
 
 		-- Those two conditions make Yadda hostile and wanting to kill tux at once.
@@ -45,10 +50,13 @@ return {
 			show("evil")
 		end
 
-		-- Yadda's wise advises
+		-- Yadda's wise advises and time-dependent events.
 		Yadda_times=Yadda_times + 1
 		if (Yadda_times==1) then
 			Npc:says(_"Come and seek my lightly advises, and you'll find out truth and light.")
+		elseif (Yadda_times==3) then
+			Npc:says(_"The only skill worth acquiring in the world: Light.")
+			show("book")
 		elseif (Yadda_times==7) then
 			Npc:says(_"Persistence is the way to perfection.")
 		elseif (Yadda_times==13) then
@@ -58,6 +66,13 @@ return {
 		elseif (Yadda_times==37) then
 			Npc:says(_"Hey, did you knew this is the 37th time you ask for my enlightenment? I'll teach you a secret. There is a mini boss on this area. Might be worth checking out.")
 			show("C64gate")
+		elseif (Yadda_times==15) then
+			Npc:says(_"I accidentally overkilled a bot and lost my Light Saber in the process. What a shame. Already built a new one, though.")
+			show("saber")
+		elseif (Yadda_times==28) then
+			Npc:says(_"I haven't seen any Alpha Bot lately. Things are strangely calm. And the calm precedes the storm.")
+			show("bots")
+		-- Random quotes. If we're not in an specific event from above, give Tux a “wise” advice.
 		elseif (Yadda_times < 20) then
 			-- All texts below are quotes adapted to FreedroidRPG world.
 			Npc:says_random(_"In matters of destroying bots, style, not brute force, is the vital thing.",
@@ -73,14 +88,14 @@ return {
 							_"Yadda Yadda Yadda.",
 							_"The secret of the sure victory is siding with Master Yadda.",
 							_"Kill bots first, ask questions later.",
-							_"There was the Experimental Alpha Class. If you meet one, your death is close.", -- Bots 0xx, relies on special skills.
+							_"There was the Experimental Alpha Class. If you meet one, your death is close.",
 							_"If you can think, you're sentient. This won't make you a life-form though.",
 							_"Lamps are nice. Always carry one with you. You never know when you'll need them.",
 							_"Bots will aim whatever is in front of them. Which, in most cases, will be you.")
 		else
 			Npc:says(_"You're very dedicated, seeking my advice, and wise words that much. But I have nothing else to tell you. Besides that: ")
 			--; TRANSLATORS: %d = number of times talked to Yadda
-			Npc:says(_"You may ask me %d times and I still won't have anything new to teach you.", Yadda_times)
+			Npc:says(_"You've asked for my wise advice %d times. Maybe you should start figuring things out on your own.", Yadda_times)
 		end
 
 
@@ -101,12 +116,133 @@ return {
 		end,
 	},
 	{
+		id = "book",
+		text = _"I want that skill you mentioned earlier.",
+		code = function()
+			Tux:says(_"Light skill, was it?")
+			Npc:says(_"Of course I can show you the wonderful program called [b]Light[/b], but I'm too lazy to actually teach you anything.", "NO_WAIT")
+			Npc:says(_"Therefore I shall write at hand a book explaining the wonders of light. But such wonderful skill will requires some readiness of mind to absorb it's rich content.")
+			--; TRANSLATORS: %d = number of training points required to buy the book.
+			Npc:says(_"For you, it'll cost only 2.000 circuits and %d training points.", Yadda_TP_cost)
+			show("buybook")
+		end,
+	},
+	{
+		id = "buybook",
+		text = _"I want to buy the Source Book of Light for the price you mentioned ealier.",
+		code = function()
+
+			-- Check if player can buy the book
+			if (Tux:get_gold() < 2000) then
+				next("no_money")
+			elseif (Tux:get_training_points() < Yadda_TP_cost) then
+				next("no_points")
+			else -- Everything is OK. Get the book.
+				Tux:del_training_points(Yadda_TP_cost)
+				Tux:add_gold(-2000)
+				Tux:add_item("Source Book of Light", 1)
+				Yadda_books_bought=Yadda_books_bought+1
+				if (Yadda_books_bought % 3 == 0) then
+					Yadda_TP_cost=Yadda_TP_cost+1
+				end
+
+				-- Fancy message
+				if (Yadda_books_bought % 3 == 1) then
+					Npc:says(_"It's a deal. May the light guide your path.")
+				elseif (Yadda_books_bought % 3 == 2) then
+					Npc:says(_"Deal. Light guide you forever.")
+				else
+					Npc:says(_"Here is the book. May the light guide your steps.")
+				end
+
+				-- Limit to 10 sells
+				if (Yadda_books_bought == 10) then
+					Npc:says(_"I won't sell you anymore from now on.")
+					hide("book")
+				elseif (Yadda_books_bought == 9) then
+					Npc:says(_"I'll sell you only one extra copy. Be sure to don't waste it.")
+				elseif (Yadda_books_bought == 7) then
+					Npc:says(_"I'll sell you only three copies more.")
+				end
+
+				hide("buybook")
+			end
+		end,
+	},
+	{
+		id = "saber",
+		text = _"You crafted a light saber? Amazing! Can I have one?",
+		code = function()
+			Npc:says(_"The light saber is for the disciples of light.")
+			Tux:says(_"I'm always eager to hear your wisdom, o wise master Yadda.")
+			Npc:says(_"I'm too wise to be fooled by your praises, young apprentice. But yet, I shall craft one to you, for a small price of my wisdom.")
+			Npc:says(_"I shall require 5.000 circuits in compensation for my efforts, 3 source books of light to prove your worthiness and eagerness to learn from my wise words... And...")
+			Tux:says(_"...And?")
+			--; TRANSLATORS: %d = number of VMX grenades required to craft the Light Saber. Varies according to difficulty.
+			Npc:says(_"...And %d VMX Gas Grenades. I'm a pacifist, but the disciples of dark - robots and viruses - wishes my life. Only with gas grenades I shall survive. They're poison to mere mortals, but for the great master Yadda, they are source of life.", Yadda_vmx_saber)
+			--; TRANSLATORS: %d = number of VMX grenades required to craft the Light Saber. Varies according to difficulty.
+			Tux:says(_"I have no idea of how life can come out from gas grenades, but I shall bring you 5.000 circs, 3 source books of light, and %d VMX grenades.", Yadda_vmx_saber)
+			show("buysaber")
+		end,
+	},
+	{
+		id = "buysaber",
+		text = _"I have the 5.000 circuits, 3 source books of light, and the VMX grenades you requested. Gimme the saber!",
+		code = function()
+
+			-- Check if player can buy the saber
+			if (Tux:get_gold() < 5000) then
+				next("no_money")
+			elseif (Tux:count_item_backpack("Source Book of Light") < 3) then
+				Npc:says(_"And the proof of eagerness? I require three source books of light.")
+			elseif (Tux:count_item_backpack("VMX Gas Grenade") < Yadda_vmx_saber) then
+			--; TRANSLATORS: %d = number of VMX grenades required to craft the Light Saber. Varies according to difficulty.
+				Npc:says(_"I really need %d VMX Gas Grenades to protect myself from the disciples of dark. I won't give you a light saber without it.", Yadda_vmx_saber)
+			else -- Everything is OK. Get the saber.
+				Tux:add_gold(-5000)
+				-- FIXME: Source Book of Light doesn't stacks. Therefore, it's necessary to call the function three times.
+				-- This is a fragility from C code to delete items. Other functions might work without this additional care.
+				Tux:del_item_backpack("Source Book of Light")
+				Tux:del_item_backpack("Source Book of Light")
+				Tux:del_item_backpack("Source Book of Light")
+				Tux:del_item_backpack("VMX Gas Grenade", Yadda_vmx_saber)
+				Tux:add_item("Light saber", 1)
+
+				Npc:says(_"Here it is.")
+				Tux:says(_"So quick? I thought you were going to craft it?")
+				Npc:says(_"I have dozens of those. I do them on my spare time.")
+
+				hide("saber", "buysaber")
+			end
+		end,
+	},
+	{
+		id = "bots",
+		text = _"Wise master Yadda, could you please instruct again this humble servant of light about what are the Alpha Bots?",
+		code = function()
+			Npc:says(_"Hah. They're all experimental, most might be scrap in a single shoot, but they all have... skills... which more than makes up for any deficiency they might have.", "NO_WAIT")
+			Npc:says(_"My friend, I hope you never meet any of them. They should be only rumors, though.")
+			Npc:says(_"Also, I heard from the bard something about Agent Zero not managing to get a good designer for these droids and reusing existing droid models on them.", "NO_WAIT") -- An excuse for missing art.
+			Npc:says(_"If this is the case, you might think you're meeting a normal bot and be surprised when it causes havoc because it's from the Alpha class.") -- Missing art can be used as extra difficulty. Still would be better to not provoke mistakes because that.
+			Npc:says(_"There is the [b]001 Influencer[/b]. It can take over your bots. If your army suddenly become hostile to you, one might be near.")
+			Npc:says(_"There is also the [b]011 Grenadier[/b]. He'll throw grenades to destroy your army. Mass destruction. Kill it as fast as you can.")
+			Npc:says(_"Not to mention the [b]021 Wizard[/b]. This one is nasty, so I hope you have an army, because it can slow you, paralyze, it can even poison you, so don't trifle with it.")
+			Tux:says(_"Sounds dangerous.")
+			Npc:says(_"Ha! Not even the start. Agent Zero did many tests on this area to create the supreme foe. There is the graveyard series: [b]Zombot[/b] and you won't want to know what it does, the [b]Ghost[/b] or [b]Shadow Hunter[/b], it uses invisibility so very dangerous.", "NO_WAIT")
+			Npc:says(_"There is also [b]Ghoul Bots[/b] and the silly [b]Pepper Bot[/b] who you won't care with when suddenly! You're dead.")
+			Tux:says(_"Sounds extremely dangerous.")
+			Npc:says(_"The alpha class carries on. They are only experiments, weak and not mass produced, so you might have a chance against them with my advises. But I must warn you that further on you might find specialized droids using those characteristics. When this happen, I hope you have The Holy Light blessings.")
+			Tux:says(_"Thank you. I shall look for one of those experimental droids to practice.")
+			Npc:says(_"...I hope you know what you're doing.")
+		end,
+	},
+	{
 		id = "evil",
 		text = _"I have this nice weapon here, want to take a look?",
 		code = function()
 			Npc:says(_"Nobody's Edge! How do you dare to bring such evil pulsing weapon to those lands!!", "NO_WAIT")
-			--; TRANSLATORS: woza futhi zisize mina → Come and aid me (Zulu, Google Translated)
-			Npc:says(_"Woza futhi zisize mina, minions! Let's kill this... this... heretic!")
+			--; TRANSLATORS: Komm und hilf mir → Come and aid me (from German)
+			Npc:says(_"Komm und hilf mir, minions! Let's kill this... this... heretic!")
 			Npc:set_faction("ms")
 			create_droid("Act2-YaddaRef1", "GUB", "ms", "AfterTakeover", "radar")
 			create_droid("Act2-YaddaRef2", "GUA", "ms", "AfterTakeover", "radar")
@@ -118,12 +254,28 @@ return {
 		text = _"I found this small cube on a desert.",
 		code = function()
 			Npc:says(_"The Pandora Box! How do you dare to bring doom to those lands!!", "NO_WAIT")
-			--; TRANSLATORS: woza futhi zisize mina → Come and aid me (Zulu, Google Translated)
-			Npc:says(_"Woza futhi zisize mina, minions! Let's kill this doom bringer!")
+			--; TRANSLATORS: Komm und hilf mir → Come and aid me (from German)
+			Npc:says(_"Komm und hilf mir, minions! Let's kill this doom bringer!")
 			Npc:set_faction("ms")
 			create_droid("Act2-YaddaRef1", "GUB", "ms", "AfterTakeover", "radar")
 			create_droid("Act2-YaddaRef2", "GUA", "ms", "AfterTakeover", "radar")
 			end_dialog()
+		end,
+	},
+	{
+		id = "no_money",
+		code = function()
+			Npc:says_random(_"Please don't bother me if you can't pay me.",
+				_"No money, no deal.",
+				_"I was pretty clear about the amount of money I wanted.")
+		end,
+	},
+	{
+		id = "no_points",
+		code = function()
+			Npc:says_random(
+				_"Come back when you have a real will to learn.",
+				_"You don't have enough experience. You'll never understand my wisdom like this.")
 		end,
 	},
 	{
