e110014de364bed598b6ab7b73f4ea633e19dd11
Jesusaves <cpntb1@ymail.com>
2016-11-03 14:51:03 -0200

    Replace the former bot which was responsible to bring player to Act 2 by a human.
    In compliance to IRC resolution taken at November 2nd.

--- data/storyline/act1/dialogs/Act2GateKeeper.lua
+++ data/storyline/act1/dialogs/Act2GateKeeper.lua
@@ -17,8 +17,8 @@
 -- MA 02111-1307 USA
 ----------------------------------------------------------------------
 --[[WIKI
-PERSONALITY = { "Robotic" },
-PURPOSE = "$$NAME$$ is part of Act2. His only purpose is to dump the player on Act 2 lands by Spencer orders."
+PERSONALITY = { "Novice" },
+PURPOSE = "$$NAME$$ is part of Act2. He is an unexperienced member who Spencer told to transport player to RR Resorts but ends up crashing the spaceship. He teaches tux to never try to pilot a spaceship when drunk."
 WIKI]]--
 
 local Npc = FDrpg.get_npc()
@@ -27,10 +27,39 @@ local Tux = FDrpg.get_tux()
 return {
 	EveryTime = function()
 		if (Act2_opengate) then
-				Npc:says(_"Hello linarian. Do you want to go to the Resorts? Please note that once you go you won't be able to come back.")
+				-- Maybe this is the first time you've met him. In such case, we must fix some storyline.
+				if (not Tux:has_met("Act2GateKeeper")) then
+					Npc:says(_"I found a drink near the cockpit, it looked OK so I drank from it.")
+					Tux:says(_"Hey, are you sure you're not drunk nor anything?")
+					Npc:says(_"Do not worry: There's nothing which could possibly go wrong!")
+				end
+				Npc:says(_"Sooo, Spencer told me to transport you to RR Resorts. Tell me when you're ready.")
 				show("node0", "node99")
+		elseif (HF_FirmwareUpdateServer_uploaded_faulty_firmware_update) then
+			Npc:says(_"Heya, congratulations!")
+			if (Tux:has_met("Act2GateKeeper")) then
+				Tux:says(_"You're still alive, it seems.")
+				Npc:says(_"Yes, I told you, there was nothing which could possibly go wrong!")
+				Tux:says(_"So, what are you doing here?")
+			else
+				Tux:says(_"Thanks. So, what's up?")
+				Npc:says(_"Oh nothing. I went to the MS bar, found a drink near some blood strains. It looked OK so I drank it.")
+				Tux:says(_"Hm...")
+				Npc:says(_"Oh, but do not worry! Nothing could possibly go wrong. Also, I have a special mission!")
+			end
+			Npc:says(_"Right now Spencer instructed me to inspect the Landing Zone. See if I find any ships.")
+			Tux:says(_"I'll let you do your work alone. Just, eh, be sure to don't drink anything you find inside the ship, ok?")
+			Npc:says(_"Ok, do not worry with it!") -- He'll drink anyway...
+			end_dialog()
 		else
-			Npc:says(_"[b]INTRUDER,[/b] leave the premises at once. You are not allowed here.")
+			--; TRANSLATORS: %s = Tux:get_player_name()
+			Npc:says(_"Hello, %s. Spencer told me to try to assist you on the Hell Fortress, but I'm scared of the bots.", Tux:get_player_name())
+			Tux:says(_"How come they haven't attacked you yet?")
+			Npc:says(_"Hmm, I'm not sure. Maybe it had something to do with that drink I found abandoned on the bar near some blood strains?")
+			Tux:says(_"You go out drinking everything you see? Really??")
+			Npc:says(_"Sure, what could possibly go wrong?")
+			Tux:says(_"...")
+			Tux:says(_"This way you'll die soon, young boy.") -- Tux refuses his help: too inexperienced.
 			end_dialog()
 		end
 	end,
@@ -39,7 +68,7 @@ return {
 		id = "node0",
 		text = _"Yes. To the next act!",
 		code = function()
-			Npc:says(_"Good luck on the unknown.")
+			Npc:says(_"Good luck on the unknown.") -- TODO: This part is to be reworked.
 			delay_game(0.2) -- for effect
 			Tux:teleport("Act2StartGameSquare") -- TODO: This must be included on endact_1() function and called here...
 			revive_faction("ms") -- fixme: This is to be removed.
