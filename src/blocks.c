oxesb/freedroidRPG

commit 3e1622e2e15235f00c355ae5497b29e28ad06080

Fix some compilation warnings (gcc 8.2.1)
This is about potential buffer overflows with strcpy() or sprint().
This patch replaces them with strncpy() and snprintf().

So other identical warnings are still to be fixed.

src/blocks.c
@@ -709,7 +709,7 @@ static struct image *get_storage_for_tux_image(const char *filename)
 void load_tux_graphics(int motion_class, int tux_part_group, const char *part_string)
 {
 	char atlas_filename[4096];
-	char atlas_directory[4096];
+	char atlas_directory[4096-10]; // '-10' == sizeof("/atlas.txt")
 	static char *part_group_strings[ALL_PART_GROUPS] = {
 		"head/",
 		"shield/",
@@ -719,9 +719,10 @@ void load_tux_graphics(int motion_class, int tux_part_group, const char *part_st
 		"weaponarm/"
 	};
 
-	sprintf(atlas_directory, "tux_motion_parts/%s/%s%s/",
-		get_motion_class_name_by_id(motion_class), part_group_strings[tux_part_group], part_string);
-	sprintf(atlas_filename, "%s/atlas.txt", atlas_directory);
+	snprintf(atlas_directory, sizeof(atlas_directory),
+	         "tux_motion_parts/%s/%s%s",
+	         get_motion_class_name_by_id(motion_class), part_group_strings[tux_part_group], part_string);
+	snprintf(atlas_filename, sizeof(atlas_filename), "%s/atlas.txt", atlas_directory);
 
 	current_tux_motion_class = motion_class;
 	current_tux_part_group = tux_part_group;

src/addon_crafting_ui.c
@@ -351,7 +351,7 @@ void show_addon_crafting_ui()
 		if (ui.materials_for_selected[i].available < ui.materials_for_selected[i].required) {
 			sprintf( text ,"%s%02d", font_switchto_red, ui.materials_for_selected[i].available);
 		} else {
-			if (ui.materials_for_selected[i].available > 100)
+			if (ui.materials_for_selected[i].available < 0 || ui.materials_for_selected[i].available > 100)
 				sprintf( text ,"%s--", font_switchto_neon);
 			else
 				sprintf( text ,"%s%02d", font_switchto_neon, ui.materials_for_selected[i].available);

src/lvledit/lvledit_menu.c
@@ -455,11 +455,12 @@ static void add_rem_level(void)
 static void level_options(void)
 {
 	char *menu_texts[100];
-	char options[20][1000];
+#	define OPTLEN 1000
+	char options[20][OPTLEN];
 	char class[16];
 	int l = 0;
 
-	class[15] = 0; // null-character to null terminated strncpy
+	class[15] = '\0'; // null-character to null terminated strncpy
 
 	enum {
 		CHANGE_LEVEL_POSITION = 1,

src/misc.c
@@ -643,9 +643,11 @@ void init_data_dirs_path()
 		FILE *f = fopen(file_path, "r");
 		if (f != NULL) {
 			// File found, so now fill the data dir paths
-			strncpy(data_dirs[DATA_ROOT].path, top_data_dir[i], PATH_MAX);
+			strncpy(data_dirs[DATA_ROOT].path, top_data_dir[i], PATH_MAX-1);
+			data_dirs[DATA_ROOT].path[PATH_MAX-1] = '\0';
 #ifdef ENABLE_NLS
-			strncpy(data_dirs[LOCALE_ROOT].path, top_locale_dir[i], PATH_MAX);
+			strncpy(data_dirs[LOCALE_ROOT].path, top_locale_dir[i], PATH_MAX-1);
+			data_dirs[LOCALE_ROOT].path[PATH_MAX-1] = '\0';
 #endif
 			fclose(f);
 			goto TOP_DIR_FOUND;

src/view.c
@@ -3064,8 +3064,8 @@ void show_inventory_screen(void)
 			set_current_font(Messagevar_Font);
 			// Only 3 characters fit in one inventory square.
 			char amount[4];
-			if (Me.Inventory[SlotNum].multiplicity < 999)
-				sprintf(amount, "%d", Me.Inventory[SlotNum].multiplicity);
+			if (Me.Inventory[SlotNum].multiplicity >= 0 && Me.Inventory[SlotNum].multiplicity <= 999)
+				sprintf(amount, "%03d", Me.Inventory[SlotNum].multiplicity);
 			else
 				strcpy(amount, "+++");
 			TargetRect.w = INV_SUBSQUARE_WIDTH * ItemMap[Me.Inventory[SlotNum].type].inv_size.x;

commit 46a7b18787e8afb62013e2392e184f236ab27c14

Fix a bug in the computation of a graphics file path, introduced by commit 3e1622e2.

src/blocks.c
@@ -709,7 +709,7 @@ static struct image *get_storage_for_tux_image(const char *filename)
 void load_tux_graphics(int motion_class, int tux_part_group, const char *part_string)
 {
 	char atlas_filename[4096];
-	char atlas_directory[4096-10]; // '-10' == sizeof("/atlas.txt")
+	char atlas_directory[4096-9]; // '-9' == sizeof("atlas.txt")
 	static char *part_group_strings[ALL_PART_GROUPS] = {
 		"head/",
 		"shield/",
@@ -720,9 +720,9 @@ void load_tux_graphics(int motion_class, int tux_part_group, const char *part_st
 	};
 
 	snprintf(atlas_directory, sizeof(atlas_directory),
-	         "tux_motion_parts/%s/%s%s",
+	         "tux_motion_parts/%s/%s%s/",
 	         get_motion_class_name_by_id(motion_class), part_group_strings[tux_part_group], part_string);
-	snprintf(atlas_filename, sizeof(atlas_filename), "%s/atlas.txt", atlas_directory);
+	snprintf(atlas_filename, sizeof(atlas_filename), "%satlas.txt", atlas_directory);
 
 	current_tux_motion_class = motion_class;
 	current_tux_part_group = tux_part_group;